c1f092a7c0518ac623dd1accc0213bef01f0993f
TusharJain86 <tusharjain0806@gmail.com>
2026-04-01 17:27:06 +0530

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='w-full bg-gradient-to-b from-white-400 to-gray-100 px-6 py-4'>navbar</div>
+  );
+}
+
+export default navbar
