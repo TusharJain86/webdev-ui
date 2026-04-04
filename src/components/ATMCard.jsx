27f6c463e6b041f315938d1847c634c182a90b99
TusharJain86 <tusharjain0806@gmail.com>
2026-04-04 23:03:21 +0530

    atm card left section completed with all details

--- src/components/ATMCard.jsx
+++ src/components/ATMCard.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-const ATMCard = ({status, accountNumber, number, expiry, cvv, }) => {
+const ATMCard = ({status, accountNumber, number, expiry, cvv, className }) => {
   return (
-    <div className="bg-gradient-to-r from-gray-900 to-gray-700 text-white rounded-2xl p-6 w-80 shadow-lg">
+    <div className={`rounded-2xl p-6 shadow-lg hover:scale-105 ${className}`}>
 
       {/* TOP SECTION */}
       <div className="flex justify-between items-start">
 
         {/* WIFI ICON */}
-        <span className="text-xl">📶</span>
+        <span className="text-xl">{status}</span>
 
         {/* STATUS */}
         <span className=""></span>
       </div>
 
       {/* CARD NUMBER */}
-      <div className="mt-6">
+      <div className="mt-3">
         <p className="text-sm text-gray-300">Account Number</p>
         <p className="text-lg tracking-widest font-semibold">
           {accountNumber}
@@ -23,12 +23,12 @@ const ATMCard = ({status, accountNumber, number, expiry, cvv, }) => {
       </div>
 
       {/* MAIN NUMBER */}
-      <p className="text-2xl tracking-[4px] mt-4 font-bold">
+      <p className="text-2xl tracking-[4px] mt-1 font-bold">
         {number}
       </p>
 
       {/* BOTTOM SECTION */}
-      <div className="flex justify-between mt-6 text-sm">
+      <div className="flex justify-between mt-1 text-sm">
 
         <div>
           <p className="text-gray-300">EXP</p>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,10 +3,22 @@ import ATMCard from '../components/ATMCard';
 
 const Home = () => {
     return (
-        <div className='p-5'>
-            <ATMCard>
-                <p className="text-green-600 text-center bg-green-300 rounded-full font-bold">Active</p>
-            </ATMCard>
+        <div className='p-5 flex gap-6 px-5'>
+            <div className='flex-2 bg-gray-200 rounded-xl p-6'>
+                <div className="grid grid-cols-2 gap-4 mt-5">
+                <ATMCard className="bg-gradient-to-r from-orange-500 to-orange-700 text-white" status="NPM BANK" accountNumber="1234 5678 9010" number="9546" expiry="12/26" cvv="800"></ATMCard>
+                <ATMCard className="bg-gradient-to-r from-gray-900 to-gray-700 text-white" status="PIN BANK" accountNumber="2222 3333 4444" number="1234" expiry="02/27" cvv="123"></ATMCard>
+                <ATMCard className="bg-black text-yellow-500" status="DOR BANK" accountNumber="3951 4456 5205" number="5678" expiry="08/27" cvv="456"></ATMCard>
+                <ATMCard className="bg-gradient-to-r from-blue-800 to-purple-900 text-white" status="ABC BANK" accountNumber="9542 7431 8562" number="2468" expiry="04/28" cvv="789"></ATMCard>
+                </div>
+            </div>
+
+            <div className="flex-3 flex flex-col gap-4">
+                {/* 3 CARDS ON TOP */}
+                <div className="grid grid-cols-3 gap-4">
+                    <p>hi</p>
+                </div>
+            </div>
         </div>
     );
 }
