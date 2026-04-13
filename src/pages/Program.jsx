dabb2935a9433b3c4a781820465308ad7735d509
TusharJain86 <tusharjain0806@gmail.com>
2026-04-14 01:24:09 +0530

    multiple pages button added program page completed

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -4,17 +4,18 @@ import axios from 'axios';
 const Program = () => {
 
     const [userData, setUserData] = useState([]);
+    const [index, setIndex] = useState(1)
     
     const getData= async()=>{
-        const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=20')
+        const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=20`)
         setUserData(response.data);
     }
 
     useEffect(function(){
         getData()
-    })
+    }, [index])
 
-    let printUserData = "No user data available"
+    let printUserData = <h3 className='text-gray-300 text lg absolute top-1/2 left-1/2 translate-x-1/2 translate-y-1/2 font-bold'>Loading...</h3>
 
     if(userData.length > 0){
         printUserData= userData.map(function(elem, idx){
@@ -29,8 +30,30 @@ const Program = () => {
 
   return (
     <div className='bg-zinc-900 overflow-auto h-screen text-white p-4'>
+        
         <div className='flex flex-wrap gap-4'>
             {printUserData}
+        </div>
+        <div className='flex justify-center items-center p-4 gap-6'>
+            <button
+            onClick={()=>{
+                if(index>1){
+                    setIndex(index-1)
+                    setUserData([])
+                }
+            }} 
+            className='bg-amber-400 rounded cursor-pointer active:scale-102 text-black font-bold px-4 py-2'>
+                Previous
+            </button>
+            <h4>Page {index}</h4>
+            <button
+            onClick={()=>{
+                setIndex(index+1)
+                setUserData([])
+            }} 
+            className='bg-amber-400 rounded cursor-pointer active:scale-102 text-black font-bold px-4 py-2'>
+                Next
+            </button>
         </div>
     </div>
   )
