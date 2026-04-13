254451a46f2de3ea3936b780e5b6ff92a9b13993
TusharJain86 <tusharjain0806@gmail.com>
2026-04-14 00:50:43 +0530

    use effect implemented to load auto data without button to get it from backend

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 const Program = () => {
@@ -6,27 +6,30 @@ const Program = () => {
     const [userData, setUserData] = useState([]);
     
     const getData= async()=>{
-        const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=30')
+        const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=20')
         setUserData(response.data);
     }
+
+    useEffect(function(){
+        getData()
+    })
 
     let printUserData = "No user data available"
 
     if(userData.length > 0){
-        printUserData= userData.map(function(){
-            return 'hello'
+        printUserData= userData.map(function(elem, idx){
+            return <div key={idx}>
+                <div className='h-40 w-45 bg-white overflow-hidden rounded-xl'>
+                    <img className='h-full w-full object-cover' src={elem.download_url} alt=""></img>
+                </div>
+                <h2 className='font-bold text-lg'>{elem.author}</h2>
+            </div>
         })
     }
 
   return (
-    <div className='bg-zinc-900 h-screen text-white p-4'>
-        <button
-        onClick={getData}
-        className='bg-green-500 px-5 py-2 rounded mb-3 active:scale-102'>
-            Get Data
-        </button>
-
-        <div>
+    <div className='bg-zinc-900 overflow-auto h-screen text-white p-4'>
+        <div className='flex flex-wrap gap-4'>
             {printUserData}
         </div>
     </div>
